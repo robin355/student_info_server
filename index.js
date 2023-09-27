e242cb747b7459f62a583d7eba845ae92b1ed841
Md.Robin Mia <robinmahmud358@gmail.com>
2023-09-27 07:39:49 -0700

    mongodb connect

--- index.js
+++ index.js
@@ -2,25 +2,42 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
-// require('dotenv').config()
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v1zucfh.mongodb.net/?retryWrites=true&w=majority`;
 
-async function run(){
-    try{
-        app.get('/', (req, res) => {
-            res.send('Hello Student')
-          })
-    }
-    finally{
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-    }
+async function run() {
+  try {
+    const studentsCollections = client.db('students').collection('student_info')
+    
+    app.get('/students', async (req, res) => {
+      const query = {}
+      const students = await studentsCollections.find(query).toArray()
+      res.send(students)
+  })
+  } finally {
+  
+  
+  }
 }
+run().catch(console.dir);
 
 
-run().catch(err => console.log(err))
+
+// run().catch(err => console.log(err))
 
 app.listen(port, () => {
   console.log(`Student Info Server Running ${port}`)
